b2d682fde157bf159944484eecd81d8632540b43
jugurj <juriyunna27@gmail.com>
2020-08-08 02:08:10 +0300

    Added proxy url call before calling the API to fix CORS headers issue.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Get quote from API
 async function getQuote() {
+    const proxyUrl = 'https://pacific-harbor-33249.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     
     try {
-        const res = await fetch(apiUrl);
+        const res = await fetch(proxyUrl + apiUrl);
         const data = await res.json();
         console.log(data);
     } catch (err) {
