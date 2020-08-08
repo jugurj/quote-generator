f0c822927f64c076b7e4f07e59428a35cb25f364
jugurj <juriyunna27@gmail.com>
2020-08-08 15:27:28 +0300

    Implemented logic to replace HTML quote placeholders with actual data coming from the API. Added event listeners and functionality to both buttons within the quote container.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Get HTML elements
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get quote from API
 async function getQuote() {
     const proxyUrl = 'https://pacific-harbor-33249.herokuapp.com/';
@@ -6,11 +13,39 @@ async function getQuote() {
     try {
         const res = await fetch(proxyUrl + apiUrl);
         const data = await res.json();
-        console.log(data);
+
+        // If from API fetching author property comes as blank string set author as unknown instead
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        // Reduce font size for longer quotes
+        if (data.quoteText.length > 80) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+
+        quoteText.innerText = data.quoteText;
     } catch (err) {
-        console.log('Oops, something went wrong.', err);
+        getQuote();
+        console.log(err);
     }
 }
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On laod
 getQuote();
